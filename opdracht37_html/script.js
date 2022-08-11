4893beb8c75093ab68aad4fd0a58f5f87d6219ff
moonstar-tech <85889077+moonstar-tech@users.noreply.github.com>
2022-08-11 12:57:13 +0200

    Add files via upload

--- opdracht37_html/script.js
+++ opdracht37_html/script.js
@@ -1,35 +1,19 @@
-  
-let toggleNavStatus = false;
+const navMenu = document.querySelector(".nav-menu")
+const navLinks = document.querySelector(".nav-links")
 
 
-let toggleNav = function() {
-    let getNavbar = document.querySelector(".navbar");
-    let getNavbarUl = document.querySelector(".navbar ul")
-    let getNavbarLinks = document.querySelectorAll(".navbar a")
-    
-   
+navMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    navLinks.classList.toggle("active");
+})
 
-    if (toggleNavStatus === false){
-        getNavbarUl.style.visibility = "visible";
-        getNavbar.style.opacity = "1";
-    
-        let arrayLength = getNavbarLinks.length;
-        for (let i = 0; i < arrayLength; i++){
-            getNavbarLinks[i].style.opacity = "1";
-        }
-        toggleNavStatus = true;
-    }
-    
+document.querySelectorAll(".nav-links").forEach(n => n.
+    addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    }))
 
-    else if(toggleNavStatus === true){
-        
-        getNavbar.style.opacity = "0";
+
     
-        let arrayLength = getNavbarLinks.length;
-        for (let i = 0; i < arrayLength; i++){
-            getNavbarLinks[i].style.opacity = "0";
-        }
-        getNavbarUl.style.visibility = "hidden";
-        toggleNavStatus = false;
-     }
+    function changeBodyBg(color){
+        document.body.style.background = color;
     }
